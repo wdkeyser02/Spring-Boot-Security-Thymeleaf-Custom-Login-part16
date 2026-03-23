e3b1fe95a810eecf4bf9c425bfd177b6bcec7ebe
Willy De Keyser <spring.boot.programming@gmail.com>
2026-03-23 09:44:50 +0100

    Setup

--- Spring-Boot-Security-Thymeleaf-Custom-Login-part16/src/main/java/willydekeyser/SpringBootSecurityThymeleafCustomLoginPart16Application.java
+++ Spring-Boot-Security-Thymeleaf-Custom-Login-part16/src/main/java/willydekeyser/SpringBootSecurityThymeleafCustomLoginPart16Application.java
@@ -2,8 +2,10 @@ package willydekeyser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity
 public class SpringBootSecurityThymeleafCustomLoginPart16Application {
 
 	public static void main(String[] args) {
